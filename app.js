fc1942a500e366c9a66588bf2bc866f2a42e751f
Siebe <r0834360@student.thomasmore.be>
2023-05-29 22:18:31 +0200

    fix: error in middleware

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 //load the donuts router
-appDonutsRouter = app.use('/api/v1/donuts', apiDonutsRouter); 
+app.use('/api/v1/donuts', apiDonutsRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
