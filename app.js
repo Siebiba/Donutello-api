4a4bd3c1c005d80220fa652ef333b2800dff6ab7
Siebe <r0834360@student.thomasmore.be>
2023-05-30 01:26:05 +0200

    fix: connection to the database

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const apiDonutsRouter  = require('./routes/api/v1/donuts');
 
 //add mongoose to the project and connect to the database 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/donutsapp', {useNewUrlParser: true});
+
+//connect to the database
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
 
 const app = express();
 
