d63a288a84756d078f63f151c454b1716482cc8d
Siebe <r0834360@student.thomasmore.be>
2023-05-29 23:16:00 +0200

    feat: add schema and model that maps a mongodb collection

--- controllers/api/v1/donuts.js
+++ controllers/api/v1/donuts.js
@@ -1,3 +1,17 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//schema for donuts collection in the database 
+const donutsSchema = new Schema({
+    text: String,
+    user: String,
+    complete: Boolean
+});
+
+//model for donuts collection with schema donutsSchema
+const Donut = mongoose.model('Donut', donutsSchema);
+
 const getAll = (req, res) => {
     res.json({
         "status": 'You are riding the donut API!'
