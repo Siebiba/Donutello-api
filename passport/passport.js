7135422e500d0b843ec615c3f2a88412fae7783a
Siebe <r0834360@student.thomasmore.be>
2023-05-30 19:07:49 +0200

    fix: create new jwt strategy with async function because callback no longer supported

--- passport/passport.js
+++ passport/passport.js
@@ -15,21 +15,18 @@ var opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'Secret';
 
-
-passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
-    User.findOne({
+//create new jwt strategy with async function
+passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
+    //find the user in db if needed
+    const user = await User.findOne({
         _id: jwt_payload.uid
-    }, function (err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            // or you could create a new account
-        }
     });
+    if (user) {
+        return done(null, user);
+    } else {
+        return done(null, false);
+    }
 }));
+
 
 module.exports = passport;
