df66430bff1d857d00b084f58c9806c4a0c95813
Siebe <r0834360@student.thomasmore.be>
2023-05-30 13:04:15 +0200

    feat: add passport local strategy and serialize user data

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const authController = require('../controllers/auth');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//signup
+  //username from request
+  //password from request
+router.get('/signup', (req, res, next) => {
+    res.send('respond with a resource');
+  });
+
+  router.post('/signup', authController.signup);
+  //router.post('/login', authController.login);
+  
+
 
 module.exports = router;
